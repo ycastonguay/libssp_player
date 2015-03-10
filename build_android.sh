e21a1813c404a7bd6e04b40c3668bb6a20d76f5c
ycastonguay <ycastonguay@mp4m.org>
2015-03-09 21:45:50 -0400

    Fixed Eclipse Java sample (player-sample-android).
    - Does not require appcompat anymore, it is now a very simple barebones
    sample project.
    - Updated Android build script.

--- build_android.sh
+++ build_android.sh
@@ -5,7 +5,8 @@ ndk-build clean
 ndk-build
 
 echo "Copying libs to sample application directories..."
-rsync --recursive libs/* player-sample-android/app/src/main/jniLibs
+rsync --recursive libs/* player-sample-android/libs
+rsync --recursive libs/* player-sample-android-gradle/app/src/main/jniLibs
 rsync --recursive libs/* player-sample-android-xamarin/lib
 
 echo "All done."
