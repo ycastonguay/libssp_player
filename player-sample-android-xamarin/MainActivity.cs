6f8d1a7f38503a5082375c80898c5b6b0fef8dd0
ycastonguay <ycastonguay@mp4m.org>
2015-03-14 16:58:40 -0400

    Xamarin.Android sample is now fully working!
    - The fix was to call manually JavaSystem.LoadLibrary on the BASS and
    libssp_player librairies.
    - The Xamarin.Android sample now has the same features as the
    Xamarin.iOS sample; it can now be used to fully test the player
    performance.

--- player-sample-android-xamarin/MainActivity.cs
+++ player-sample-android-xamarin/MainActivity.cs
@@ -22,8 +22,11 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
-using org.sessionsapp.player;
 using System.Timers;
+using org.sessionsapp.player;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace playersampleandroidxamarin
 {
@@ -35,19 +38,42 @@ namespace playersampleandroidxamarin
         private StateChangedDelegate _stateChangedDelegate;
         private PlaylistIndexChangedDelegate _playlistIndexChangedDelegate;
 
+        private TextView _lblVersion;
+        private TextView _lblState;
+        private TextView _lblPlaylist;
+        private TextView _lblFilePath;
+        private TextView _lblPosition;
+        private Button _btnPlay;
+        private Button _btnPause;
+        private Button _btnStop;
+        private Button _btnPrevious;
+        private Button _btnNext;
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
             SetContentView(Resource.Layout.Main);
-            Button button = FindViewById<Button>(Resource.Id.myButton);
-            button.Click += delegate
-            {
-                //button.Text = string.Format("{0} clicks!", count++);
-            };
 
-            InitializeBASS();
-            //InitializePlayer();
+            _lblVersion = FindViewById<TextView>(Resource.Id.lblVersion);
+            _lblState = FindViewById<TextView>(Resource.Id.lblState);
+            _lblPlaylist = FindViewById<TextView>(Resource.Id.lblPlaylist);
+            _lblFilePath = FindViewById<TextView>(Resource.Id.lblFilePath);
+            _lblPosition = FindViewById<TextView>(Resource.Id.lblPosition);
+
+            _btnPlay = FindViewById<Button>(Resource.Id.btnPlay);
+            _btnPlay.Click += HandleBtnPlayClick;
+            _btnPause = FindViewById<Button>(Resource.Id.btnPause);
+            _btnPause.Click += HandleBtnPauseClick;
+            _btnStop = FindViewById<Button>(Resource.Id.btnStop);
+            _btnStop.Click += HandleBtnStopClick;
+            _btnPrevious = FindViewById<Button>(Resource.Id.btnPrevious);
+            _btnPrevious.Click += HandleBtnPreviousClick;
+            _btnNext = FindViewById<Button>(Resource.Id.btnNext);
+            _btnNext.Click += HandleBtnNextClick;
+
+            InitializePlayer();
+            LoadAudioFiles();
 
             _timerRefreshPosition = new Timer();
             _timerRefreshPosition.Interval = 100;
@@ -56,64 +82,34 @@ namespace playersampleandroidxamarin
 
         private void HandleTimerRefreshPositionElapsed(object sender, ElapsedEventArgs e)
         {
-//            var position = new SSPPosition();
-//            SSP.SSP_GetPosition(ref position.Struct);
-//
-//            InvokeOnMainThread(() => {
-//                lblPosition.StringValue = string.Format("Position: {0}", position.Str);
-//            });
+            var position = new SSPPosition();
+            SSP.SSP_GetPosition(ref position.Struct);
+
+            RunOnUiThread(() => {
+                _lblPosition.Text = string.Format("Position: {0}", position.Str);
+            });
         }
-
-        private void InitializeBASS()
-        {
-            Console.WriteLine("libssp_player - Initializing BASS test...");
-            int success = BASS.BASS_Init(-1, 44100);
-            Console.WriteLine("libssp_player - Initializing BASS test (success: {0})... DONE!", success);
-        }
-
-//        private void InitializePlayer()
-//        {
-//            int version = SSP.SSP_GetVersion();
-//            Console.WriteLine("libssp_player - Version: {0}", version);
-//
-//            Console.WriteLine("libssp_player - Initializing player...");
-//            int error = SSP.SSP_Init(string.Empty);
-//            if (error != SSP.SSP_OK)
-//            {
-//                Console.WriteLine("libssp_player - SSP_Init error code: {0}", error);
-//                return;
-//            }
-//
-//            Console.WriteLine("libssp_player - Initializing device...");
-//            error = SSP.SSP_InitDevice(-1, 44100, 1000, 100, false);
-//            if (error != SSP.SSP_OK)
-//            {
-//                Console.WriteLine("libssp_player - SSP_InitDevice error code: {0}", error);
-//                return;
-//            }
-//
-//            Console.WriteLine("libssp_player - Init successful!");
-//        }
-
+            
         private void InitializePlayer()
         {
+            // Load dynamic libraries
+            Java.Lang.JavaSystem.LoadLibrary("bass");
+            Java.Lang.JavaSystem.LoadLibrary("bass_fx");
+            Java.Lang.JavaSystem.LoadLibrary("bassmix");
+            Java.Lang.JavaSystem.LoadLibrary("bassenc");
+            Java.Lang.JavaSystem.LoadLibrary("ssp_player");
+
             // Perform any additional setup after loading the view, typically from a nib.
             int version = SSP.SSP_GetVersion();
-            //lblVersion.StringValue = string.Format("Version {0}", version);
+            _lblVersion.Text = string.Format("Version {0}", version);
             Console.WriteLine("libssp_player version: {0}", version);
 
-//            // Try to get the plugins in the current path
-//            string exePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-//            string pluginPath = exePath.Replace("MonoBundle", "Resources");
-//            if(!File.Exists(pluginPath + "/libbassflac.dylib"))
-//            {
-//                throw new Exception("The BASS plugins could not be found in the current directory!");
-//            }
-
-            int error = SSP.SSP_Init(string.Empty);
+            string path = ApplicationInfo.NativeLibraryDir;
+            int error = SSP.SSP_Init(path);
             if (error != SSP.SSP_OK)
             {
                 Console.WriteLine("libssp_player init failed with error code: {0}", error);
+                CheckForError(error);
                 return;
             }
 
@@ -124,17 +120,41 @@ namespace playersampleandroidxamarin
             SSP.SSP_SetStateChangedCallback(_stateChangedDelegate, IntPtr.Zero);
             SSP.SSP_SetPlaylistIndexChangedCallback(_playlistIndexChangedDelegate, IntPtr.Zero);
 
-            error = SSP.SSP_InitDevice(-1, 44100, 1000, 100, true);
+            error = SSP.SSP_InitDevice(-1, 44100, 1000, 100, false);
             if (error != SSP.SSP_OK)
             {
                 Console.WriteLine("libssp_player init device failed with error code: {0}", error);
+                CheckForError(error);
                 return;
             }
 
-//            var device = new SSPDevice();
-//            SSP.SSP_GetDevice(ref device.Struct);
+            var device = new SSPDevice();
+            SSP.SSP_GetDevice(ref device.Struct);
+
+            int deviceCount = SSP.SSP_GetOutputDeviceCount();
+            var devices = new List<SSPDevice>();
+            for (int a = 0; a < deviceCount; a++)
+            {
+                var newDevice = new SSPDevice();
+                SSP.SSP_GetOutputDevice(a, ref newDevice.Struct);
+                devices.Add(newDevice);
+            }
 
             Console.WriteLine("libssp_player init successful!");
+        }
+
+        private void LoadAudioFiles()
+        {
+            SSP.SSP_Playlist_Clear();
+            var musicFolder = global::Android.OS.Environment.GetExternalStoragePublicDirectory(global::Android.OS.Environment.DirectoryMusic).ToString();
+            string[] extensions = { ".mp3", ".flac", ".ape", ".wav", ".ogg", ".mpc", ".wv" };
+            var files = Directory.EnumerateFiles(musicFolder, "*.*", SearchOption.AllDirectories)
+                .Where(s => extensions.Any(ext => ext == Path.GetExtension(s)));
+            foreach (string file in files)
+            {
+                Console.WriteLine("File: {0}", file);
+                SSP.SSP_Playlist_AddItem(file);
+            }
         }
 
         private void CheckForError(int error)
@@ -151,26 +171,55 @@ namespace playersampleandroidxamarin
         }
 
         private void HandleStateChanged(IntPtr user, SSPPlayerState state)
-        {
-//            InvokeOnMainThread(() => {
-//                if(lblState != null)
-//                    lblState.StringValue = string.Format("State: {0}", state);
-//            });
+        {            
+            RunOnUiThread(() => {
+                _lblState.Text = string.Format("State: {0}", state);
+            });
         }
 
         private void HandlePlaylistIndexChanged(IntPtr user)
         {
-//            int index = SSP.SSP_Playlist_GetCurrentIndex();
-//            int count = SSP.SSP_Playlist_GetCount();
-//            var item = new SSPPlaylistItem();
-//            SSP.SSP_Playlist_GetItemAt(index, ref item.Struct);
-//
-//            InvokeOnMainThread(() => {
-//                if(lblPlaylist != null)
-//                    lblPlaylist.StringValue = string.Format("Playlist [{0}/{1}]", index+1, count);
-//                if(lblFilePath != null)
-//                    lblFilePath.StringValue = string.Format("File path: {0}", item.FilePath);
-//            });
+            int index = SSP.SSP_Playlist_GetCurrentIndex();
+            int count = SSP.SSP_Playlist_GetCount();
+            var item = new SSPPlaylistItem();
+            SSP.SSP_Playlist_GetItemAt(index, ref item.Struct);
+
+            RunOnUiThread(() => {
+                _lblPlaylist.Text = string.Format("Playlist [{0}/{1}]", index+1, count);
+                _lblFilePath.Text = string.Format("File path: {0}", item.FilePath);
+            });
         }
+
+        private void HandleBtnPlayClick(object sender, EventArgs e)
+        {
+            CheckForError(SSP.SSP_Play());
+
+            if(!_timerRefreshPosition.Enabled)
+                _timerRefreshPosition.Start();  
+        }
+
+        private void HandleBtnPauseClick(object sender, EventArgs e)
+        {
+            CheckForError(SSP.SSP_Pause());
+        }
+
+        private void HandleBtnStopClick(object sender, EventArgs e)
+        {
+            CheckForError(SSP.SSP_Stop());
+
+            if(_timerRefreshPosition.Enabled)
+                _timerRefreshPosition.Stop();
+        }
+
+        private void HandleBtnPreviousClick(object sender, EventArgs e)
+        {
+            CheckForError(SSP.SSP_Previous());
+        }
+
+        private void HandleBtnNextClick(object sender, EventArgs e)
+        {
+            CheckForError(SSP.SSP_Next());
+        }
+
     }
 }
--- player-sample-android-xamarin/Resources/Resource.designer.cs
+++ player-sample-android-xamarin/Resources/Resource.designer.cs
@@ -60,8 +60,35 @@ namespace playersampleandroidxamarin
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050009
+			public const int btnNext = 2131034121;
+			
+			// aapt resource value: 0x7f050006
+			public const int btnPause = 2131034118;
+			
+			// aapt resource value: 0x7f050005
+			public const int btnPlay = 2131034117;
+			
+			// aapt resource value: 0x7f050008
+			public const int btnPrevious = 2131034120;
+			
+			// aapt resource value: 0x7f050007
+			public const int btnStop = 2131034119;
+			
+			// aapt resource value: 0x7f050003
+			public const int lblFilePath = 2131034115;
+			
+			// aapt resource value: 0x7f050002
+			public const int lblPlaylist = 2131034114;
+			
+			// aapt resource value: 0x7f050004
+			public const int lblPosition = 2131034116;
+			
+			// aapt resource value: 0x7f050001
+			public const int lblState = 2131034113;
+			
 			// aapt resource value: 0x7f050000
-			public const int myButton = 2131034112;
+			public const int lblVersion = 2131034112;
 			
 			static Id()
 			{
